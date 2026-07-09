12a37d260851a00380a8aea9296f736b9956ee65
Luís Felipe Albuquerque Fernandes <luisfelipeaf31@gmail.com>
2026-07-09 20:27:31 -0300

    Arrumando "testes"

--- rodar_testes.py
+++ rodar_testes.py
@@ -1,11 +1,3 @@
-"""Executa toda a suíte de testes do projeto (pasta testes/).
-
-Uso (na raiz do projeto):
-    python rodar_testes.py
-
-Equivale a rodar: python -m unittest discover -s testes -v
-"""
-
 import unittest
 
 if __name__ == "__main__":
